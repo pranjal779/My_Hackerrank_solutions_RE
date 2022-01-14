36e67d2f734f5843f45ff600ad18ea3fb8162628
Pranjal Shukla <pranjal779@gmail.com>
2022-01-13 22:47:36 -0500

    adding Nested list solution and logic

---
+++ Nested list/logic and understanding/chk.py
@@ -0,0 +1,23 @@
+# if __name__ == '__main__':
+#     for _ in range(int(input())):
+#         name = input()
+#         score = float(input())
+#
+# res = []
+# grade = []
+#
+# res.append([name, score])
+# grade.append(score)
+#
+# grade = sorted(set(grade))
+#
+# m = grade[1]
+#
+# name = []
+# for value in res:
+#     if m == value[1]:
+#         name.append(value[0])
+#
+# name.sort()
+# for nm in name:
+#     print(nm)
