19808f049b9ad3093b0755cf6164a6d0e0ac10df
Pranjal Shukla <pranjal779@gmail.com>
2024-12-08 01:12:54 +0530

    Write_a_Func_leap_year02

---
+++ Python/from Dec 2024/Write_a_Func_leap_year02.py
@@ -0,0 +1,30 @@
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                leap = True
+            leap = True
+
+    return leap
+
+year = int(input())
+print(is_leap(year))
+
+# 2/6 test cases failed :(
+
+# Test case 1
+
+# Test case 5
+
+# Test case 0
+
+# Test case 2
+
+# Test case 3
+
+# Test case 4
+# Compiler Message
+# Wrong Answer
