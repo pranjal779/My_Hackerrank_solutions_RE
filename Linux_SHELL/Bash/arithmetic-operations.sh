752c7e1b3377e735f0803d0172cb1a50a361b767
Pranjal Shukla <pranjal779@gmail.com>
2025-01-11 20:20:14 +0530

    Create arithmetic-operations.sh

---
+++ Linux_SHELL/Bash/arithmetic-operations.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+read input
+
+printf "%.3f\n" `echo "$input" | bc -l`
+
+
+# Read The Read me, and
+# https://www.hackerrank.com/challenges/bash-tutorials---arithmetic-operations/tutorial
